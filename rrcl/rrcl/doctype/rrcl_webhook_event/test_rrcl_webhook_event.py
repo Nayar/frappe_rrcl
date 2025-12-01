e8d6227f7188d7f408f3318911bc951bac14d77c
Nayar Joolfoo <nayar@joolfoo.com>
2025-12-01 07:34:55 +0000

    renamed event

--- rrcl/rrcl/doctype/rrcl_webhook_event/test_rrcl_webhook_event.py
+++ rrcl/rrcl/doctype/rrcl_webhook_event/test_rrcl_webhook_event.py
@@ -13,9 +13,9 @@ IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 
 
 
-class IntegrationTestRRCLWebHookReceiver(IntegrationTestCase):
+class IntegrationTestRRCLWebHookEvent(IntegrationTestCase):
 	"""
-	Integration tests for RRCLWebHookReceiver.
+	Integration tests for RRCLWebHookEvent.
 	Use this class for testing interactions between multiple components.
 	"""
 
