1e261a2d2febdfcd1138ed9a1cef2ddcba73c5c7
Nayar Joolfoo <nayar@joolfoo.com>
2025-12-22 10:28:53 +0000

    add stock

--- rrcl/rrcl/doctype/rrcl_scaffolding_request/rrcl_scaffolding_request.py
+++ rrcl/rrcl/doctype/rrcl_scaffolding_request/rrcl_scaffolding_request.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2025, LEFINTECH LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class RRCLScaffoldingRequest(Document):
-	pass
+	def before_save(self):
+
+		# Iterate through the child table rows (table_yngh)
+		for row in self.table_yngh:
+			print(row.item)
+			stock_details = frappe.get_all(
+				'RRCL Stock', 
+				filters={'item': row.item}, 
+				fields=['qty']
+			)
+			if(len(stock_details)):
+				print(stock_details)
+
+				# Add the fetched available qty to the total
+				row.available_in_store = stock_details[0]['qty']
