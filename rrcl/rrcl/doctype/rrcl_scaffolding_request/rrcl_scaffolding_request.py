81361648ec16699ecc66085d81cbd1005d8a6989
Nayar Joolfoo <nayar@joolfoo.com>
2026-01-09 11:09:16 +0000

    add rental cost in scaffolding

--- rrcl/rrcl/doctype/rrcl_scaffolding_request/rrcl_scaffolding_request.py
+++ rrcl/rrcl/doctype/rrcl_scaffolding_request/rrcl_scaffolding_request.py
@@ -7,10 +7,14 @@ from frappe.model.document import Document
 
 class RRCLScaffoldingRequest(Document):
 	def before_save(self):
-
+		self.rental_per_day = 0
 		# Iterate through the child table rows (table_yngh)
 		for row in self.table_yngh:
 			print(row.item)
+			try:
+				self.rental_per_day += float(row.qty * float(frappe.get_doc("RRCL Item",row.item).rental_price))
+			except:
+				pass
 			stock_details = frappe.get_all(
 				'RRCL Stock', 
 				filters={'item': row.item}, 
@@ -21,3 +25,4 @@ class RRCLScaffoldingRequest(Document):
 
 				# Add the fetched available qty to the total
 				row.available_in_store = stock_details[0]['qty']
+			
