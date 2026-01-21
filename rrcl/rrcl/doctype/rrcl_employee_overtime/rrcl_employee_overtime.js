5c4eee791c180358a269b1b216fea5bb44f21ef1
Nayar Joolfoo <nayar@joolfoo.com>
2026-01-21 07:17:43 +0000

    default times

--- rrcl/rrcl/doctype/rrcl_employee_overtime/rrcl_employee_overtime.js
+++ rrcl/rrcl/doctype/rrcl_employee_overtime/rrcl_employee_overtime.js
@@ -6,3 +6,17 @@
 
 // 	},
 // });
+frappe.ui.form.on('RRCL Employee Overtime Item', { // Replace with your actual child table DocType name
+    table_wiyb_add: function(frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn);
+        if (frm.doc.default_overtime_start) {
+            frappe.model.set_value(cdt, cdn, 'overtime_start', frm.doc.default_overtime_start);
+        }
+        if (frm.doc.default_overtime_end) {
+            frappe.model.set_value(cdt, cdn, 'overtime_end', frm.doc.default_overtime_end);
+        }
+        if (frm.doc.default_overtime_hrs) {
+            frappe.model.set_value(cdt, cdn, 'overtime_hrs', frm.doc.default_overtime_hrs);
+        }
+    }
+});
