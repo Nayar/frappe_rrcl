fddd3207d34d6d4148cdb865215dfed6b299f815
Nayar Joolfoo <nayar@joolfoo.com>
2026-01-22 10:34:12 +0000

    work site

--- rrcl/rrcl/doctype/rrcl_site_timesheet/rrcl_site_timesheet.py
+++ rrcl/rrcl/doctype/rrcl_site_timesheet/rrcl_site_timesheet.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2026, LEFINTECH LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class RRCLSiteTimesheet(Document):
-	pass
+	def on_submit(self):
+		parent_timesheet = frappe.get_doc("RRCL Timesheet", self.parent_timesheet)
+
+		count = 0
+		for row in self.table_employees:
+			for parent_row in parent_timesheet.table_employees:
+				if(row.employee == parent_row.employee and parent_row.status != row.status):
+					parent_row.status = row.status
+					count += 1
+		if(count > 0):
+			parent_timesheet.save()
